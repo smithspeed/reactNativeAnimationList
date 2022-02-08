f32efe844cfacad939b2a76d4e089cf510ad7f93
Dhananjay <thecryptodeveloper@gmail.com>
2022-02-08 21:05:50 +0530

    upds

---
+++ application/controllers/Home.php
@@ -0,0 +1,20 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Home extends CI_Controller {
+
+	
+	public function index()
+	{	
+		$data = new stdClass();
+
+		$this->load->view('template/header.php',$data);
+		$this->load->view('home/home_index.php',$data);
+		$this->load->view('template/footer.php',$data);
+	}
+
+	public function dx()
+	{
+		$this->load->view('welcome_message');
+	}
+}
